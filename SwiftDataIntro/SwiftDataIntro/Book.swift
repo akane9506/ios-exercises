2eda2ec6dd4f56157479ea1d67ad37bf43a1708c
akane9506 <akane19950611@gmail.com>
2025-09-06 23:37:27 -0700

    swift data initialized

--- SwiftDataIntro/SwiftDataIntro/Book.swift
+++ SwiftDataIntro/SwiftDataIntro/Book.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+class Book {
+    var title: String
+    var author: String
+    var dateAdded: Date
+    var dateStarted: Date
+    var summary: String
+    var rating: Int?
+    var status: Status
+    
+    init(
+        title: String,
+        author: String,
+        dateAdded: Date,
+        dateStarted: Date,
+        status: Status,
+        summary: String,
+        rating: Int? = nil
+    ) {
+        self.title = title
+        self.author = author
+        self.dateAdded = dateAdded
+        self.dateStarted = dateStarted
+        self.summary = summary
+        self.status = status
+        self.rating = rating
+    }
+}
+
+enum Status: Int, Codable, Identifiable, CaseIterable {
+    case onShelf, inProgress, completed
+    
+    var id: Self {
+        self
+    }
+    
+    var descr: String {
+        switch self {
+        case .onShelf:
+            "On Shelf"
+        case .inProgress:
+            "In Progress"
+        case .completed:
+            "Completed"
+        }
+    }
+}
